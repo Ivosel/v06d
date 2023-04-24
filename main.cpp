0e06baafdcee2d51dade762a0addaff0258fa461
Ivor Novosel <ivnovosel@vsite.hr>
2023-04-24 20:58:40 +0200

    Finished the assignment

--- main.cpp
+++ main.cpp
@@ -2,28 +2,80 @@
 #include <cmath>
 #include "rc.h"
 
+COLORREF get_color(HWND parent, COLORREF cur) {
+	COLORREF custom_colors[16]{ 0 };
+	CHOOSECOLOR cc{ sizeof CHOOSECOLOR };
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	cc.hwndOwner = parent;
+	cc.lpCustColors = custom_colors;
+	cc.rgbResult = cur;
+	if (::ChooseColor(&cc))
+		cur = cc.rgbResult;
+	return cur;
+}
+
 int number_dialog::idd() const {
 	return IDD_NUMBER;
 }
 bool number_dialog::on_init_dialog() {
+	set_int(IDC_EDIT1, num);
 	return true;
 }
 bool number_dialog::on_ok() {
+	try {
+		num = get_int(IDC_EDIT1);
+	}
+	catch (std::exception) {
+		return false;
+	}
 	return true;
 }
 
-void main_window::on_paint(HDC hdc) {
-}
 
 void main_window::on_command(int id){
 	switch(id){
-		case ID_COLOR: 
+		case ID_COLOR:
+			color = get_color(*this, color);
 			break;
-		case ID_NUMBER: 
+
+		case ID_NUMBER: {
+			number_dialog nd;
+			nd.num = number;
+			if (nd.do_modal(::GetModuleHandle(NULL), *this) == IDOK) {
+			number = nd.num;
+			}
+		}
 			break;
+
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
+	}
+	::InvalidateRect(*this, 0, true);
+}
+
+void main_window::on_paint(HDC hdc) {
+	RECT rc; ::GetClientRect(*this, &rc);
+	draw_circles(hdc, rc);
+	
+}
+
+void main_window::draw_circles(HDC hdc, RECT rc) {
+	pen p(color, 1, PS_NULL);
+	sel_obj sp(hdc, p);
+	brush b(color);
+	sel_obj sb(hdc, b);
+	SetROP2(hdc, R2_NOTXORPEN);
+	int centerX = rc.right / 2;
+	int centerY = rc.bottom / 2;
+	int circleRadius = rc.bottom / 4;
+	double radius = min(centerX, centerY) - circleRadius;
+
+	double angleStep = 360. / number ;
+	for (int i = 0; i < number; i++) {
+		int x = centerX + radius * cos(i * angleStep * 3.14159265 / 180);
+		int y = centerY - radius * sin(i * angleStep * 3.14159265 / 180);
+		Ellipse(hdc, x - circleRadius, y - circleRadius, x + circleRadius, y + circleRadius);
 	}
 }
 
--- main.h
+++ main.h
@@ -6,11 +6,43 @@ protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+public:
+	int num;
 };
 
 class main_window : public vsite::nwp::window {
+	COLORREF color{RGB(0,0,0)};
+	int number{9};
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+	void draw_circles(HDC hdc, RECT rc);
+};
+
+class pen {
+	HPEN h;
+public:
+	pen(COLORREF color, int width = 1, int style = PS_SOLID) :
+		h(::CreatePen(style, width, color)) {}
+	~pen() { ::DeleteObject(h); }
+	operator HPEN() { return h; }
+};
+
+class brush {
+	HBRUSH h;
+public:
+	brush(COLORREF color, int hatch = -1) :
+		h(hatch >= 0 ? CreateHatchBrush(hatch, color) : ::CreateSolidBrush(color)) {}
+	~brush() { ::DeleteObject(h); }
+	operator HBRUSH() { return h; }
+};
+
+class sel_obj {
+	HDC hdc;
+	HGDIOBJ hOld;
+public:
+	sel_obj(HDC hdc, HGDIOBJ hObj) :
+		hdc(hdc), hOld(::SelectObject(hdc, hObj)) { }
+	~sel_obj() { ::SelectObject(hdc, hOld); }
 };
